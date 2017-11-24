4447d98e2332e91e5717d86599946101bbe7f71c
Simon de Lang <simondelang@gmail.com>
2017-11-24 10:09:23 +0100

    Add babel plugin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var babel = require('babel-core');
 
-let result = babel.transform('const a = 15;');
+let result = babel.transform('let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };', { babelrc: true });
 console.log(result.code);
