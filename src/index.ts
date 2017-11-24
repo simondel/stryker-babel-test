0ee182d839bceec52bc7ba52d9a7688dce191528
Sander Koenders <sanderkoenders@gmail.com>
2017-11-24 12:56:59 +0100

    Remove spread operator

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import * as babylon from "babylon"
+import * as babylon from 'babylon';
 import generate from 'babel-generator';
+import traverse from 'babel-traverse';
 
-const code = `var a = ['a', 'b', 'c']; var b = [...a, 'foo'];`;
+const code = `const a = ['a', 'b', 'c']; const b = [a, 'foo'];`;
 const ast = babylon.parse(code);
+
+traverse(ast, {
+    enter(path) {
+        if(path.isSpreadElement()) {
+            console.log(path.node = path.node.argument);
+        }
+    }
+});
 
 console.log(generate(ast));
